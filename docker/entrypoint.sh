f03015f79c5ff5ced872fd90e6af1c0357439672
Scotchy <sarradro@gmail.com>
2022-03-06 18:28:47 +0100

    gunicorn is now used in docker official image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,4 +6,4 @@ export LANG=C.UTF-8
 mkdir -p /data/artifacts && mkdir -p /data/mongodb
 
 nohup mongod --dbpath /data/mongodb --port 27017&
-xpipe --db-host 127.0.0.1 --db-port 27017 --host 0.0.0.0 --port 80 --artifacts-dir /data/artifacts
+xpipe --db-host 127.0.0.1 --db-port 27017 --host 0.0.0.0 --port 80 --artifacts-dir /data/artifacts --gunicorn --workers ${WORKERS:-2}
