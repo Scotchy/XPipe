4dcad03b792a27825e5b5671b84dd5b70ffe5e70
Scotchy <sarradro@gmail.com>
2022-10-30 22:02:17 +0100

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/x-rst',
     packages=find_packages(),
-    version = "1.0.0",
+    version = "1.0.1",
     description="Standardize your ML projects",
     author="Jules Tevissen",
     license="MIT",
